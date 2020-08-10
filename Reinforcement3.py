0239f1f5172f36bc4b2b1da85411c99eb6e7ee27
237sankalp <66549964+237sankalp@users.noreply.github.com>
2020-08-10 23:05:52 +0530

    Add files via upload

---
+++ Reinforcement3.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+R=np.matrix([[-1,0,0,0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+             [0,-1,-1,0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+             [0,-1,-1,-1,-1,0,0,-1,-1,-1,-1,-1,-1,-1,-1],
+             [0,0,-1,-1,0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+             [-1,-1,-1,0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+             [-1,-1,0,-1,-1,-1,0,-1,-1,0,-1,-1,-1,-1,-1],
+             [-1,-1,0,-1,-1,0,-1,0,0,-1,-1,-1,-1,-1,-1],
+             [-1,-1,-1,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1,-1],
+             [-1,-1,-1,-1,-1,-1,0,-1,-1,0,0,-1,-1,-1,-1],
+             [-1,-1,-1,-1,-1,0,-1,-1,0,-1,-1,-1,-1,-1,-1],
+             [-1,-1,-1,-1,-1,-1,-1,-1,0,-1,-1,100,0,-1,-1],
+             [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,0,100,-1,0,-1],
+             [-1,-1,-1,-1,-1,-1,-1,0,-1,-1,0,-1,-1,0,-1],
+             [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,100,0,-1,0],
+             [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,0,-1]])
+
+Q=np.matrix(np.zeros([15,15]))
+
+gamma=0.8
+
+initial_state=1
+
+def available_action(state):
+    current_state=R[state]
+    av_act=np.where(current_state>=0)[1]
+    return av_act
+
+def next_action(ava_action):
+    next_act=int(np.random.choice(ava_action,1))
+    return next_act
+
+def update_matrix(current,gamma,action):
+    c=0
+    A= available_action(action)
+    for i in range(len(A)):
+        if Q[action,A[i]]>=c:
+            c=Q[action,A[i]]
+        else:
+            c=c
+    Q[current,action]=R[current,action]+gamma*c
+
+
+for i in range(10000):
+    current=np.random.randint(0,15)
+    available_act=available_action(current)
+    action=next_action(available_act)
+    update_matrix(current,gamma,action)
+
+#print(Q/np.max(Q)*100)
+
+current_step=3
+steps=[current_step]
+
+while current_step != 11:
+    next_step=np.where(Q[current_step,]==np.max(Q[current_step,]))[1]
+    if next_step.shape[0]>1:
+        next_step=int(np.random.choice(next_step,size=1))
+    else:
+        next_step=int(next_step)
+    steps.append(next_step)
+    current_step=next_step
+
+print(steps)
